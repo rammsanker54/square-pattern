04b84671ebef0686e6a84a05ccb9d91767c643c7
RammSanker <109334253+rammsanker54@users.noreply.github.com>
2022-10-27 15:13:52 +0530

    squarepattern

---
+++ square.js
@@ -0,0 +1,11 @@
+var number = 4; 
+let square = "";
+
+for(let i = 0; i < number; i++) { 
+    
+  for(let j = 0; j < number; j++) { 
+    square += " + ";
+  }
+ square += "\n";
+}
+console.log(square);
